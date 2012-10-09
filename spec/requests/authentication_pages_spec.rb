606102ecd118780a4f01dd9e0a0b72f9b50a23b1
Anthony M <ajmoye@hal-tec.com>
2012-10-09 09:28:23 -0400

    Complete Index and Pagination

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "AuthenticationPages" do
+describe "Authentication" do
 
   subject { page }
   
@@ -35,6 +35,7 @@ describe "AuthenticationPages" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Users',       href: users_path) }
       it { should_not have_link('Sign in', href: signin_path) }
       
       describe "followed by signout" do
@@ -90,6 +91,11 @@ describe "AuthenticationPages" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
         end
+        
+        describe "visiting the user index" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end        
       end
     end
 
